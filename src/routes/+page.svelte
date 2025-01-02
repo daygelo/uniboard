b5d45f18b45aca27f148f29d3d5b7f3afc306b22
Angelo Djeumene <80226633+daygelo@users.noreply.github.com>
2025-01-02 12:17:47 -0600

    Responsible design tweaks.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,24 +25,24 @@
 </script>
 
 <main class='w-full min-h-screen bg-[#a86f48] bg-noise'>
-  <section class='mx-auto w-full max-w-screen-xl py-16'>
-    <div class='flex gap-2 max-w-3xl mx-auto mb-16'>
+  <section class='mx-auto w-full max-w-screen-xl px-4 sm:px-8 py-16'>
+    <div class='flex max-sm:flex-col gap-2 max-w-3xl mx-auto mb-16'>
       <div class='flex items-center gap-2 grow pl-4 bg-white rounded-md shadow-sm shadow-black/10'>
         <MagnifyingGlass class='size-6' weight='bold'/>
         <input
           class='w-full h-12 outline-none lg:text-xl'
-          placeholder='Search programs, universities and scholarships...'
+          placeholder='Search "Software Engineering"...'
           bind:value={input}
         />
       </div>
       <Select.Root bind:value={selectedCategories} multiple>
-        <Select.Trigger class='flex items-center w-60 pl-4 pr-3 bg-white rounded-md shadow-sm shadow-black/10'>
+        <Select.Trigger class='flex items-center sm:w-60 min-h-10 pl-4 pr-3 bg-white rounded-md shadow-sm shadow-black/10'>
           <span class='grow text-left lg:text-xl text-ellipsis font-medium overflow-hidden whitespace-nowrap'>
             {selectedCategories.length > 0 ? selectedCategories.join(', ') : 'All'}
           </span>
           <CaretDown class='shrink-0 size-4' weight='bold'/>
         </Select.Trigger>
-        <Select.Content class='mt-1 bg-white rounded-md divide-y divide-stone-200 shadow-sm shadow-black/10 z-10'>
+        <Select.Content class='mt-1 w-[calc(100vw-32px)] sm:w-60 bg-white rounded-md divide-y divide-stone-200 shadow-sm shadow-black/10 z-10'>
           {#each categoryKeys as category}
             {@const Icon = categories[category].icon}
             <div class='p-1'>
